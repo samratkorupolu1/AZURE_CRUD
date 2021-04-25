e99b9021499cb7f3a2b051573a2bb8fb6f82c11b
Dharmarakshit Harley <dharmarakshit@usf.edu>
2021-04-25 18:27:53 -0400

    Comments_3

--- Startup.cs
+++ Startup.cs
@@ -63,7 +63,7 @@ namespace Assignment4
             }
             else
             {
-                //
+                //Error handling
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
